7e065b21d6ed21f6c915ae42dcca26d118f148e5
Krisztián Horváth <krisz.202120@gmail.com>
2025-04-06 21:37:21 +0200

    Controllers done

---
+++ Raktar/Controllers/UsersController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Raktar.Services;
+using static Raktar.Dtos.UserDto;
+
+namespace Raktar.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserReadDto>>> GetAll()
+        {
+            var users = await _userService.GetAllAsync();
+            return Ok(users);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserReadDto>> GetById(int id)
+        {
+            var user = await _userService.GetByIdAsync(id);
+            return user == null ? NotFound() : Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserReadDto>> Create(UserCreateDto dto)
+        {
+            var createdUser = await _userService.CreateAsync(dto);
+            return CreatedAtAction(nameof(GetById), new { id = createdUser.Id }, createdUser);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, UserCreateDto dto)
+        {
+            var result = await _userService.UpdateAsync(id, dto);
+            return result ? NoContent() : NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _userService.DeleteAsync(id);
+            return result ? NoContent() : NotFound();
+        }
+    }
+}
